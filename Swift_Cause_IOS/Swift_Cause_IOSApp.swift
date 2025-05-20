396c6b01055cb6120c89ec99665a74bc47971d09
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-05-20 23:41:57 +0530

    Create Swift_Cause_IOSApp.swift

---
+++ Swift_Cause_IOS/Swift_Cause_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Cause_IOSApp.swift
+//  Swift_Cause_IOS
+//
+//  Created by Yash Raghuvanshi on 20/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Cause_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
