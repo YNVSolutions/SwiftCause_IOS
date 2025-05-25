befd8c3391e007fa71f61a508a2fa6d6ff984d9d
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-05-25 17:04:40 +0530

    adding basic outlet to the page

--- Swift_Cause_IOS/ContentView.swift
+++ Swift_Cause_IOS/ContentView.swift
@@ -2,21 +2,50 @@
 //  ContentView.swift
 //  Swift_Cause_IOS
 //
-//  Created by Yash Raghuvanshi on 20/05/25.
+//  Created by Yash Raghuvanshi.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Swift Cause")
-                .bold()
+        ZStack {
+            Color.blue.opacity(0.1)
+                .ignoresSafeArea()
+
+            VStack {
+                Spacer()
+
+                Text("SwiftCause")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.blue)
+                    .padding(.bottom, 10)
+                
+                Text("Empowering your passion for positive change.")
+                    .font(.title2)
+                    .foregroundColor(.gray)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal)
+                    .padding(.bottom, 30)
+
+      
+                Text("SwiftCause helps you discover, support, and connect with causes that matter most to you. Make a difference, one swift action at a time.")
+                    .font(.body)
+                    .foregroundColor(.black)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 20)
+                    .padding(.bottom, 50)
+
+        
+                
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
