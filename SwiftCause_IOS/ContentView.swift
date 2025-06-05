acfe7b09e4ae7118149142c90492d5bac2f8b098
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-06-05 18:45:46 +0530

    Update ContentView.swift

--- SwiftCause_IOS/ContentView.swift
+++ SwiftCause_IOS/ContentView.swift
@@ -1,25 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftCause_IOS
+//
+//  Created by Yash Raghuvanshi on 05/06/25.
+//
+
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showLogin = false
-
     var body: some View {
         VStack {
-            Text("Welcome to SwiftCause")
-                .font(.largeTitle)
-                .padding()
-
-            Button("Login") {
-                showLogin = true
-            }
-            .font(.title2)
-            .padding()
-            .sheet(isPresented: $showLogin) {
-                LoginView()
-            }
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
+        .padding()
     }
 }
-#Preview{
+
+#Preview {
     ContentView()
 }
