55a6895cdfd7bd774748ddf3eb748f047c9b41a1
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-06-07 16:13:37 +0530

    integrating firebase to the app

--- SwiftCause_IOS/SwiftCause_IOSApp.swift
+++ SwiftCause_IOS/SwiftCause_IOSApp.swift
@@ -1,7 +1,12 @@
 import SwiftUI
+import FirebaseCore
 
 @main
 struct SwiftCause_IOSApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
