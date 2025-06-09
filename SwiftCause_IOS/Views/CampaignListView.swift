01b602f11b6ef3b810235de6aad7059d13a47125
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-06-09 23:15:48 +0530

    using the view model fetching the data

--- SwiftCause_IOS/Views/CampaignListView.swift
+++ SwiftCause_IOS/Views/CampaignListView.swift
@@ -1,9 +1,29 @@
 import SwiftUI
 
 struct CampaignListView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @StateObject private var vm = CampaignListViewModel()
+
+  var body: some View {
+    ScrollView {
+      VStack(spacing: 20) {
+        Text("current_campaigns")
+          .font(.largeTitle)
+          .fontWeight(.bold)
+          .padding(.bottom, 10)
+
+        ForEach(vm.items) { item in
+          DonationRowView(item: item) {
+            vm.donate(to: item)
+          }
+        }
+      }
+      .padding(.bottom, 20)
     }
+    .task {
+      await vm.fetchCampaigns()
+    }
+    .navigationBarTitleDisplayMode(.inline)
+  }
 }
 
 #Preview {
