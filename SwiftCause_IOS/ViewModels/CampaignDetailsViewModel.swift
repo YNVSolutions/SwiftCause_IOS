c881c0904ea8be48d25d8f653bd95215719ea579
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-07-04 09:42:13 +0530

    Remove unused CampaignDetailsViewModel class

--- SwiftCause_IOS/ViewModels/CampaignDetailsViewModel.swift
+++ SwiftCause_IOS/ViewModels/CampaignDetailsViewModel.swift
@@ -1,10 +1,1 @@
-import Foundation
-import Combine
 
-class CampaignDetailsViewModel: ObservableObject {
-    let campaign: Campaign
-
-    init(campaign: Campaign) {
-        self.campaign = campaign
-    }
-}
