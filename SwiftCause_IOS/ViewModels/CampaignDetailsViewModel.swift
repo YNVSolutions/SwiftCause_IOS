d245bb2cfaeab8dddfc3fe4eaf48b862a7c36b7c
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-07-16 15:30:26 +0530

    initiizing the model

--- SwiftCause_IOS/ViewModels/CampaignDetailsViewModel.swift
+++ SwiftCause_IOS/ViewModels/CampaignDetailsViewModel.swift
@@ -1,10 +1,14 @@
 import Foundation
-import Combine
 
 class CampaignDetailsViewModel: ObservableObject {
-    let campaign: Campaign
-
-    init(campaign: Campaign) {
+    @Published var campaign: DonationItem
+    
+    init(campaign: DonationItem) {
         self.campaign = campaign
+    }
+    
+    func donate() {
+        print("User donated to: \(campaign.title) with amount \(campaign.amount)")
+        // Adding donation logic here
     }
 }
