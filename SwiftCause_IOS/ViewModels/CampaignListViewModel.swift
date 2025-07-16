f5ce3e4a4c75b028c8050d493414e2b11f7f18dd
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-07-16 17:03:33 +0530

    Update CampaignListViewModel.swift

--- SwiftCause_IOS/ViewModels/CampaignListViewModel.swift
+++ SwiftCause_IOS/ViewModels/CampaignListViewModel.swift
@@ -2,9 +2,4 @@ import Foundation
 
 class CampaignListViewModel: ObservableObject {
     @Published var donationItems: [DonationItem] = CampaignData.donationItems
-    
-    func donate(to item: DonationItem) {
-        print("User wants to donate to: \(item.title) with amount \(item.amount)")
-        // Adding additional donation logic here of firebase
-    }
 }
