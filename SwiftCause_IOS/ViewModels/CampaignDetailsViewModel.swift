ebf842efac9701c7bf4a77b387dd23ad5a6db076
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-07-16 17:03:28 +0530

    Update CampaignDetailsViewModel.swift

--- SwiftCause_IOS/ViewModels/CampaignDetailsViewModel.swift
+++ SwiftCause_IOS/ViewModels/CampaignDetailsViewModel.swift
@@ -6,9 +6,4 @@ class CampaignDetailsViewModel: ObservableObject {
     init(campaign: DonationItem) {
         self.campaign = campaign
     }
-    
-    func donate() {
-        print("User donated to: \(campaign.title) with amount \(campaign.amount)")
-        // Adding donation logic here
-    }
 }
